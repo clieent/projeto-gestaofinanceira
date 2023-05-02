6046edaebca6aa99df7425d182feb2683ddca319
Diego Reis <diego.reeis26@gmail.com>
2023-05-02 20:03:37 -0300

    Erro resolvido
    Erro de salvar usuario no banco resolvido

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,20 +1,24 @@
-import {Prop, SchemaFactory} from '@nestjs/mongoose'
-import { Document } from 'mongoose'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Date, Document } from 'mongoose'
 
 export type IUser = User & Document
 
+@Schema({ timestamps: true, collection: 'users' })
 export class User {
-    @Prop({required: true})
+    @Prop({ required: true })
     name: string
 
-    @Prop({required: true})
+    @Prop({ required: true })
     email: string
 
-    @Prop({required: true})
+    @Prop({ required: true })
     phone: string
 
-    @Prop({required: true})
+    @Prop({ required: true })
     cpf: string
+
+    @Prop({ defult: null, type: Date })
+    deleted_at
 }
 
 export const UserSchema = SchemaFactory.createForClass(User)
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,35 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import mongoose, { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
-import { User } from './entities/user.entity';
+import { Injectable } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import mongoose, { Model } from 'mongoose'
+import { InjectModel } from '@nestjs/mongoose'
+import { User } from './entities/user.entity'
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel("users") private readonly users: Model<User>) {}
-  async create(createUserDto: CreateUserDto) {
-    const user = await this.users.create(createUserDto)
-    console.log(createUserDto)
-    if(!user) {
-      return 'Erro ao criar o usuário!'
+    constructor(@InjectModel('users') private readonly users: Model<User>) {}
+    async create(createUserDto) {
+        const user = await this.users.create({
+            name: createUserDto?.name ?? '',
+            email: createUserDto?.email ?? '',
+            phone: createUserDto?.phone ?? '',
+            cpf: createUserDto?.cpf ?? '',
+        })
+        console.log(createUserDto)
+        if (!user) {
+            return 'Erro ao criar o usuário!'
+        }
+        return user
     }
-    return user
-  }
 
-  async findAll() {
-    return await this.users.find().exec()
-  }
+    async findAll() {
+        return await this.users.find().exec()
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    async findOne(id: string) {
+        return `This action returns a #${id} user`
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    async update(id: string, updateUserDto) {
+        await this.users.findByIdAndUpdate(id, updateUserDto).exec()
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+    async remove(id: string) {
+        await this.users.findByIdAndRemove(id)
+    }
 }
