2963d2b6645b2055d56ededd0cc4207a507eecdf
Diego Reis <diego.reeis26@gmail.com>
2023-05-09 08:41:01 -0300

    Feita a criptografia da senha

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,17 +4,19 @@ import { UpdateUserDto } from './dto/update-user.dto'
 import mongoose, { Model } from 'mongoose'
 import { InjectModel } from '@nestjs/mongoose'
 import { User } from './entities/user.entity'
+import {hash} from 'bcrypt'
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel('users') private readonly users: Model<User>) {}
     async create(createUserDto) {
+        const passwordCrypto = await hash(createUserDto.password, 8)
         const user = await this.users.create({
             name: createUserDto?.name ?? '',
             email: createUserDto?.email ?? '',
             phone: createUserDto?.phone ?? '',
             cpf: createUserDto?.cpf ?? '',
-            password: createUserDto?.password ?? '',
+            password: passwordCrypto ?? '',
         })
         console.log(createUserDto)
         if (!user) {
